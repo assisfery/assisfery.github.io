1541adcc552db9aa5032989adafb04a46ce4bba7
Assis Ferreira <ferreira.assisruny@gmail.com>
2021-12-06 22:25:56 -0100

    skills data from json

--- js/script.js
+++ js/script.js
@@ -5,3 +5,42 @@
 //         $('#mobile-menu').offcanvas('hide');
 //     }, 500);
 // }
+
+var app = null;
+
+$(document).ready(function(){
+
+    app = new Vue({
+        el: '#app',
+    
+        data: {
+    
+            message: 'Hello Vue!',
+
+            loadingSkills: false,
+            skills: [],
+    
+        },
+    
+        methods: {
+
+            getSkills: function() {
+
+                app.loadingSkills = true;
+                $.get("api/skills.json", function(data){
+                    app.skills = data;
+
+                    setTimeout(function(){
+                        app.loadingSkills = false;
+                    }, 500);
+                });
+                
+            }
+
+        },
+    
+    });
+
+    app.getSkills();
+
+});
